e598293fa130f7592b9b3548ae352f65f3333283
Stanislav Jakuschevskij <stas@two-giants.com>
2024-11-29 19:43:32 +0100

    Simplified test messages

--- examples/storage/stack_test.go
+++ examples/storage/stack_test.go
@@ -9,11 +9,11 @@ func Test_CanCreateStack(t *testing.T) {
 	stack := storage.NewStack()
 
 	if stack == nil {
-		t.Fatalf("expected a stack but got %v", stack)
+		t.Fatal("expected a stack but got", stack)
 	}
 
 	if !stack.IsEmpty() {
-		t.Fatalf("expected empty check to be 'true' but got '%t'", stack.IsEmpty())
+		t.Fatal("expected empty check to be 'true' but got", stack.IsEmpty())
 	}
 }
 
@@ -27,7 +27,7 @@ func Test_afterOnePush_isNotEmpty(t *testing.T) {
 	}
 
 	if stack.Size() != 1 {
-		t.Fatalf("stack size should be 1 but it is: %d", stack.Size())
+		t.Fatal("stack size should be 1 but it is", stack.Size())
 	}
 }
 
@@ -42,7 +42,7 @@ func Test_afterOnePushAndPop_isEmpty(t *testing.T) {
 	}
 
 	if stack.Size() != 0 {
-		t.Fatalf("stack size should be 0 but it is: %d", stack.Size())
+		t.Fatal("stack size should be 0 but it is", stack.Size())
 	}
 }
 
@@ -53,14 +53,14 @@ func Test_afterTwoPushes_sizeIsTwo(t *testing.T) {
 	stack.Push(0)
 
 	if stack.Size() != 2 {
-		t.Fatalf("stack size should be 2 but is: %d", stack.Size())
+		t.Fatal("stack size should be 2 but is", stack.Size())
 	}
 }
 
 func Test_poppingEmptyStack_panicsWithUnderflow(t *testing.T) {
 	defer func() {
 		if v := recover(); v.(string) != "underflow" {
-			t.Fatal("should panic with an 'underflow' message but did with: ", v)
+			t.Fatal("should panic with an 'underflow' message but did with", v)
 		}
 	}()
 
@@ -78,13 +78,13 @@ func Test_afterPushingX_willPopX(t *testing.T) {
 
 	popped := stack.Pop()
 	if popped != 99 {
-		t.Fatal("should have popped 99 but popped: ", popped)
+		t.Fatal("should have popped 99 but popped", popped)
 	}
 
 	stack.Push(88)
 	popped = stack.Pop()
 	if popped != 88 {
-		t.Fatal("should have popped 88 but popped: ", popped)
+		t.Fatal("should have popped 88 but popped", popped)
 	}
 }
 
@@ -96,11 +96,11 @@ func Test_afterPushingXandY_willPopYthenX(t *testing.T) {
 
 	popped := stack.Pop()
 	if popped != 88 {
-		t.Fatal("first pop should've been 88 but was: ", popped)
+		t.Fatal("first pop should've been 88 but was", popped)
 	}
 
 	popped = stack.Pop()
 	if popped != 99 {
-		t.Fatal("last pop should've been 99 but was: ", popped)
+		t.Fatal("last pop should've been 99 but was", popped)
 	}
 }
